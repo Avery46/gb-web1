b0f6a9673bfb8bc993e79f1bb82894076e7d19aa
Avery46 <kursakovegor46@gmail.com>
2022-03-22 20:43:25 +0300

    les4zan

--- kursakov_egor_dz_4/task_4_1.py
+++ kursakov_egor_dz_4/task_4_1.py
@@ -12,13 +12,13 @@ class YandexParser:
         news_list = []
         response = requests.get(self.host, headers=self.headers)
         dom = html.fromstring(response.text)
-        news_elements = dom.xpath("//div[contains(@class,'news-top-stories')]/div")
+        news_elements = dom.xpath("//div[contains(@class,'mg-grid__row mg-grid__row_gap_8 news-top-flexible-stories news-app__top')]/div")
         for element in news_elements:
             news = {
-                'title': str(element.xpath(".//h2/text()")[0]),
-                'url':  str(element.xpath(".//a[@class='news-card__link']/@href")[0]),
-                'source': str(element.xpath(".//span[@class='mg-card-source__source']/a/text()")[0]),
-                'publication_date': str(element.xpath(".//span[@class='mg-card-source__time']/text()")[0])
+                'title': str(element.xpath(".//a[@class='mg-card__link']/text()")),
+                'url':  str(element.xpath(".//a[@class='mg-card__link']/@href")[0]),
+                'source': str(element.xpath(".//span[@class='mg-card-source__source']/a/text()")),
+                'publication_date': str(element.xpath(".//span[@class='mg-card-source__time']/text()"))
             }
             news_list.append(news)
         return news_list
@@ -29,6 +29,6 @@ db = client['news']
 
 news_result = YandexParser().parse()
 
-db.news.insert_one(news_result)
+db.news.insert_many(news_result)
 
 pprint(news_result)
